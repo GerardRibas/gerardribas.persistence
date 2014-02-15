a4e40459933279a08570d131089dc476cd226f89
Gerard Ribas Canals <gerard.ribas.canals@gmail.com>
2014-02-15 20:45:39 +0000

    Upgraded versions of:   - Spring Framework 4.0.0   - Hibernate Core 4.3.0   - JPA Api 2.1

--- src/main/java/es/gerardribas/persistence/dao/impl/AbstractJpaDaoImpl.java
+++ src/main/java/es/gerardribas/persistence/dao/impl/AbstractJpaDaoImpl.java
@@ -1,80 +1,79 @@
 /**
- * 
+ *
  */
 package es.gerardribas.persistence.dao.impl;
 
-import java.io.Serializable;
-import java.util.List;
+import es.gerardribas.persistence.dao.AbstractJpaDao;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import es.gerardribas.persistence.dao.AbstractJpaDao;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author Gerard Ribas Canals (gerard.ribas.canals@gmail.com)
- * 
  */
 @SuppressWarnings("serial")
 public abstract class AbstractJpaDaoImpl<T extends Serializable, E> implements
-		AbstractJpaDao<T, E> {
+        AbstractJpaDao<T, E> {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    private EntityManager entityManager;
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public T findById(Class<T> claz, E primaryKey) {
-		return entityManager.find(claz, primaryKey);
-	}
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public T findById(Class<T> claz, E primaryKey) {
+        return entityManager.find(claz, primaryKey);
+    }
 
-	@Transactional(propagation = Propagation.REQUIRED)
-	public T update(T entity) {
-		return entityManager.merge(entity);
-	}
+    @Transactional(propagation = Propagation.REQUIRED)
+    public T update(T entity) {
+        return entityManager.merge(entity);
+    }
 
-	@Transactional(propagation = Propagation.REQUIRED)
-	public void persist(T entity) {
-		entityManager.persist(entity);
-	}
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void persist(T entity) {
+        entityManager.persist(entity);
+    }
 
-	@Transactional(propagation = Propagation.REQUIRED)
-	public void remove(T entity) {
-		T newEntity = entityManager.merge(entity);
-		entityManager.remove(newEntity);
-	}
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void remove(T entity) {
+        T newEntity = entityManager.merge(entity);
+        entityManager.remove(newEntity);
+    }
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<T> findAll(Class<T> claz) {
-		return findAll(claz, null, null);
-	}
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public List<T> findAll(Class<T> claz) {
+        return findAll(claz, null, null);
+    }
 
-	@SuppressWarnings("unchecked")
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<T> findAll(Class<T> claz, Integer startPosition,
-			Integer maxResult) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("FROM ").append(claz.getSimpleName());
-		Query query = entityManager.createQuery(sb.toString());
+    @SuppressWarnings("unchecked")
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public List<T> findAll(Class<T> claz, Integer startPosition,
+                           Integer maxResult) {
+        Query query = entityManager.createQuery("FROM " + claz.getSimpleName());
 
-		if (startPosition != null) {
-			query.setFirstResult(startPosition);
-		}
+        if (startPosition != null) {
+            query.setFirstResult(startPosition);
+        }
 
-		if (maxResult != null) {
-			query.setMaxResults(maxResult);
-		}
+        if (maxResult != null) {
+            query.setMaxResults(maxResult);
+        }
 
-		return query.getResultList();
-	}
+        return query.getResultList();
+    }
 
-	/**
-	 * @return the entityManager
-	 */
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
+    /**
+     * @return the entityManager
+     */
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 }
--- src/test/java/es/gerardribas/persistence/dao/impl/BillDaoImpl.java
+++ src/test/java/es/gerardribas/persistence/dao/impl/BillDaoImpl.java
@@ -1,28 +1,20 @@
 /**
- * 
+ *
  */
 package es.gerardribas.persistence.dao.impl;
-
-import javax.persistence.EntityManager;
-
-import org.springframework.stereotype.Repository;
 
 import es.gerardribas.persistence.dao.BillDao;
 import es.gerardribas.persistence.domain.Bill;
 import es.gerardribas.persistence.domain.Customer;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Gerard Ribas Canals (gerard.ribas.canals@gmail.com)
- * Dao for {@link Customer} table
+ *         Dao for {@link Customer} table
  */
 @Repository
 public class BillDaoImpl extends AbstractJpaDaoImpl<Bill, Long> implements BillDao {
 
-	private static final long serialVersionUID = 1L;
-	
-	@Override
-	public EntityManager getEntityManager() {
-		return super.getEntityManager();
-	}
+    private static final long serialVersionUID = 1L;
 
 }
